a6514c96f126d2ced922c92ef213f52024827f7a
Hassan <129509657+HassanCie@users.noreply.github.com>
2023-06-04 22:27:34 +0100

    Passage au GIT

---
+++ assets/js/notifications.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+	let body = document.body;
+	let notificationScreen = document.createElement("div");
+	notificationScreen.id = "notifications";
+	notificationScreen.style;
+})
---
+++ script.js
@@ -0,0 +1,61 @@
+function deb (f) {
+	let frame;
+	return (...params) => {
+		if (frame) {
+			cancelAnimationFrame(frame);
+		}
+		frame = requestAnimationFrame(() => {
+			f(...params);
+		});
+
+	}
+};
+
+function storeScroll () {
+	document.body.setAttribute("scroll", window.scrollY);
+}
+
+
+function copyText(text) {
+	let element = document.querySelector("#_copyText");
+	if (!element) {
+		element = document.createElement("input");
+		element.id = "_copyText";
+		element.style.display = "none";
+	}
+
+	element.value = text;
+	element.select();
+	element.setSelectionRange(0, text.length);
+	
+	navigator.clipboard.writeText(element.value);
+}
+
+document.addEventListener("readystatechange", (event) => {
+	if (localStorage.getItem("theme") === null) {
+		localStorage.setItem("theme", (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"));
+	}
+
+	if (localStorage.getItem("theme") == "dark") document.body.setAttribute("darkMode", true)
+
+	document.addEventListener('scroll', deb(storeScroll), { passive: true });
+	storeScroll();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	document.getElementById("themeChanger").onclick = function () {
+		document.body.toggleAttribute("darkMode")
+		localStorage.setItem("theme", (document.body.hasAttribute("darkMode") ? "dark" : "light"))
+	}
+
+	document.querySelectorAll("h1").forEach(function (el) {
+		el.onclick = function () {
+			
+			let url = new URL(location.href);
+			url.search = "";
+			url.hash = this.id;
+			window.location.hash = this.id;
+			copyText(url.toString());
+		}
+	})
+})
